0a3316905ef58bb572785ca3814df13f98a6de13
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-07-14 14:13:22 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,8 +35,7 @@ FROM users
 INNER JOIN pledges 
 ON users.id = pledges.user_id
 GROUP BY users.name
-ORDER BY 
-"
+ORDER BY total_pledges"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
