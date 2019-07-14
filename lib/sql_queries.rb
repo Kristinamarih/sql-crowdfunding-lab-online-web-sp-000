00f8d7aa85d19169a0008a73357a3a1d081596eb
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-07-14 14:16:37 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -42,7 +42,6 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 "SELECT projects.category, pledges.amount FROM projects
 INNER JOIN pledges
 ON projecs.id = pledges.project_id
-GROUP BY projects.category
 WHERE category = 'music';"
 end
 
