45c570c32d52c3d8fa97c7b63a76c2fa6f588340
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-07-14 14:18:47 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -46,5 +46,8 @@ WHERE category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"SELECT projects.category, SUM(pledges.amount)"
+"SELECT projects.category, SUM(pledges.amount) AS total_pledge FROM projects
+INNER JOIN pledges 
+ON projects.category = pledges.project_id
+"
 end
