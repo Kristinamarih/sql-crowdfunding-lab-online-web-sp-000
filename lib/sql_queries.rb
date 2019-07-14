4f4210d942c2aedec8479e8ce21100002fdf59fe
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-07-14 13:23:12 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT title.projects, amount.pledges
  FROM projects, pledges
- ORDER BY title.projects "
+ ORDER BY(title.projects)"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
